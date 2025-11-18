9089ccb253c09728f79c2cfdb4b4a9d90ad62627
A. <143023565+TilMorrow@users.noreply.github.com>
2025-11-17 21:06:51 -0800

    Delete front-end directory

--- front-end/src/Components/VideoMessageBox.js
+++
@@ -1,9 +0,0 @@
-const VideoMessage = ({message}) => {
-    if(message){
-        return <div className="call-info"> <h1>{message}</h1></div>
-    } else{
-        return <></>
-    }
-}
-
-export default VideoMessage
